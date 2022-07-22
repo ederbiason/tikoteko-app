36ecb7023928d0271b83cf2191c5fa136b7964ee
Eder Henrique Biason de Oliveira <82395795+ederbiason@users.noreply.github.com>
2022-07-22 16:41:53 -0300

    Video card
    Done the video card component that contains the video buttons, play and mute, pause and unmute
    And the buttons just appear when the user put the mouse on the video card

---
+++ components/NoResults.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { NextPage } from 'next';
+
+interface IProps {
+    text: string;
+}
+
+export const NoResults: NextPage<IProps> = ({ text }) => {
+  return (
+    <div>
+
+    </div>
+  )
+}
