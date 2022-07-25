bfa22997c378065c5164c9e48f9a04c13942e647
Eder Henrique Biason de Oliveira <82395795+ederbiason@users.noreply.github.com>
2022-07-25 15:52:35 -0300

    Google Auth
    Done the google auth and login
    At first I was supposed to use react google login, but this tool will stop to work next year, so Google recommend to use the Google Identity Services

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,16 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Image from 'next/image'
 import Link from 'next/link'
-import { useRouter } from 'next/router'  
-import { GoogleLogin, GoogleLogout } from 'react-google-login'
+import { useRouter } from 'next/router' 
+
+import { GoogleLogin, googleLogout } from '@react-oauth/google'
 
 import Logo from '../utils/tikoteko-logo.png'
 
 import { AiOutlineLogout } from 'react-icons/ai'
 import { BiSearch } from 'react-icons/bi'
 import { IoMdAdd } from 'react-icons/io'
+import { createOrGetUser } from '../utils'
+
+import useAuthStore from '../store/authStore'
 
 export const Navbar = () => {
+  const user = false
+
   return (
     <div className="w-full flex justify-between items-center border-b-2 border-gray-200 py-2 px-4">
         <Link href="/">
@@ -23,6 +29,25 @@ export const Navbar = () => {
                 />
             </div>
         </Link>
+
+        <div>
+          Search
+        </div>
+
+        <div>
+          {user ? (
+            <div>Logged In</div>
+          ): (
+            <GoogleLogin 
+              onSuccess={(response) => {
+                createOrGetUser(response);
+              }}
+              onError={() => {
+                console.log('Error')
+              }}
+            />
+          )}
+        </div>
     </div>
   )
 }
