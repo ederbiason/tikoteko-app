48e577f76dc4f6774d5543d9106f6486c8bdaf2c
Eder Henrique Biason de Oliveira <82395795+ederbiason@users.noreply.github.com>
2022-07-25 16:13:14 -0300

    Creating user store
    Created the login and logout system and
    the user store using zustand to store informations when the user makes the login

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import Image from 'next/image'
 import Link from 'next/link'
-import { useRouter } from 'next/router' 
+import { useRouter } from 'next/router'
 
 import { GoogleLogin, googleLogout } from '@react-oauth/google'
 
@@ -15,39 +15,70 @@ import { createOrGetUser } from '../utils'
 import useAuthStore from '../store/authStore'
 
 export const Navbar = () => {
-  const user = false
+  const { userProfile, addUser, removeUser } = useAuthStore()
 
   return (
     <div className="w-full flex justify-between items-center border-b-2 border-gray-200 py-2 px-4">
-        <Link href="/">
-            <div className="w-[100px] md:w-[130px]">
-                <Image 
-                    className="cursor-pointer"
-                    src={Logo}
-                    alt="TikoTeko"
-                    layout='responsive'
-                />
-            </div>
-        </Link>
-
-        <div>
-          Search
+      <Link href="/">
+        <div className="w-[100px] md:w-[130px]">
+          <Image
+            className="cursor-pointer"
+            src={Logo}
+            alt="TikoTeko"
+            layout='responsive'
+          />
         </div>
+      </Link>
 
-        <div>
-          {user ? (
-            <div>Logged In</div>
-          ): (
-            <GoogleLogin 
-              onSuccess={(response) => {
-                createOrGetUser(response);
+      <div>
+        Search
+      </div>
+
+      <div>
+        {userProfile ? (
+          <div className="flex gap-5 md:gap-10">
+            <Link href='/upload'>
+              <button className="border-2 px-2 md:px-4 text-md font-semibold flex items-center gap-2">
+                <IoMdAdd className="text-xl" /> {` `}
+                <span className="hidden md:block">Upload</span>
+              </button>
+            </Link>
+            {userProfile.image && (
+              <Link href="/">
+                <>
+                  <Image
+                    width={40}
+                    height={40}
+                    className="rounded-full cursor-pointer"
+                    src={userProfile.image}
+                    alt="profile photo"
+                  />
+                </>
+              </Link>
+            )}
+
+            <button 
+              className="px-2"
+              type="button"
+              onClick={() => {
+                googleLogout()
+                removeUser()
               }}
-              onError={() => {
-                console.log('Error')
-              }}
-            />
-          )}
-        </div>
+            >
+              <AiOutlineLogout color="red" fontSize={21} />
+            </button>
+          </div>
+        ) : (
+          <GoogleLogin
+            onSuccess={(response) => {
+              createOrGetUser(response, addUser);
+            }}
+            onError={() => {
+              console.log('Error')
+            }}
+          />
+        )}
+      </div>
     </div>
   )
 }
